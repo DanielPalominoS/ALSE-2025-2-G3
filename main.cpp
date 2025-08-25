6bcbe5fca77d76c55585470f4f650a4e40b4ca0b
DanielPalomino <daniel.palomino@mail.escuelaing.edu.co>
2025-08-25 12:52:36 -0500

    third commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h> // Include the standard input/output library for printf
+#include <stdio.h>
+// Main function: entry point of the program
+int main(int argc, char** argv ){
+	// Print 'Hello World.' to the console
+	printf("Hello World.\n" );
+	// Return 0 to indicate successful execution
+	return 0;
+}
